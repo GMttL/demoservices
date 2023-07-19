0a2490581151b51f8ac32695b7491742c2f85ebe
GMttL <gaby.mititelu@gmail.com>
2023-07-19 21:01:33 +0300

    Fraud Microservice Project Setup

---
+++ fraud-microservice/src/main/java/com/gabrielmttl/fraud/FraudApplication.java
@@ -0,0 +1,12 @@
+package com.gabrielmttl.fraud;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApplication {
+
+        public static void main(String[] args) {
+            org.springframework.boot.SpringApplication.run(FraudApplication.class, args);
+        }
+}
